a737370f04541de2d91caaebe3fff9fa372a1e71
Priyati Jain <75193975+Priyati1@users.noreply.github.com>
2023-09-17 18:43:13 +0530

    Add files via upload

---
+++ good_sub_arrays.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+#define ll long long 
+ll a[1000005],prefix[1000005];
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		for(int i=0;i<1000005;i++){
+			prefix[i]=0;
+		}
+		ll sum=0;
+		prefix[0]=1;
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+			sum +=a[i];
+			sum=sum%n;
+			sum=(sum+n)%n;//negative values get converted into positive 
+			prefix[sum]++;
+
+		}
+		ll ans=0;
+		for(int i=0;i<n;i++){
+			ll no=prefix[i];
+			ans+=(((no)*(no-1))/2);
+		}
+		cout<<ans<<endl;
+
+	}
+
+	return 0;
+}
